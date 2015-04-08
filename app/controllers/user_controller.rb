6f544269059f3d8751ba08d7a2a5e451ecbc3c89
Hyeon Kim <simnalamburt@gmail.com>
2015-04-09 06:47:11 +0900

    이상할경우 에러

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -4,16 +4,33 @@ class UserController < ApplicationController
     user.username = params[:id]
     user.password = params[:passwd]
     user.count = 1
-    user.save
+    if user.save
+      render json: {
+        user_name: user.username,
+        login_count: user.count
+      }
+    else
+      msg = user.errors.messages
 
-    render json: {
-      user_name: user.username,
-      login_count: user.count
-    }
+      e1 = msg[:username] && msg[:username] != ['has already been taken']
+      e2 = !msg[:password].nil?
+      e3 = msg[:username] && msg[:username] == ['has already been taken']
+
+      render json: {
+        error_code:
+          e1 ? -1 :
+          e2 ? -2 :
+          e3 ? -3 : 0
+      }
+    end
   end
 
   def login
-    user = User.where(username: params[:id]).first()
+    user = User.where(username: params[:id], password: params[:passwd]).first()
+    unless user
+      return render json: { error_code: -4 }
+    end
+
     user.count += 1
     user.save
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+  validates :username, length: { minimum: 5, maximum: 20 }
+  validates :password, length: { minimum: 8, maximum: 20 }
+  validates_uniqueness_of :username
 end
