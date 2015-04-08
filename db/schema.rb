6388c1e3fb849ad27ecef4e587c1f19bdc05b420
Hyeon Kim <simnalamburt@gmail.com>
2015-04-09 04:52:08 +0900

    유저네임 칼럼을 유니크하게 만들었음

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150408190033) do
+ActiveRecord::Schema.define(version: 20150408194255) do
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
@@ -20,5 +20,7 @@ ActiveRecord::Schema.define(version: 20150408190033) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
 end
