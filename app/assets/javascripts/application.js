504f87dad3a1241ae43b94baa7371901a12efa1d
Hyeon Kim <simnalamburt@gmail.com>
2015-04-09 15:35:13 +0900

    CSRF 검증 끔

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,11 +2,3 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-
-$(function() {
-  $.ajaxSetup({
-    headers: {
-      'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')
-    }
-  });
-});
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,6 @@
 class UserController < ApplicationController
+  skip_before_filter :verify_authenticity_token
+
   def signup
     user = User.new
     user.username = params[:id]
